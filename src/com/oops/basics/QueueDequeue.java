c32baaab9c0443752e17d5cb542a0204cd0c7499
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-08 21:11:08 +0530

    Added : UC-2 Using Linked List for DeQueue Operation

---
+++ src/com/oops/basics/QueueDequeue.java
@@ -0,0 +1,34 @@
+package com.oops.basics;
+
+public class QueueDequeue 
+{
+	LinkedList list = new LinkedList();
+	public void enQueue(Object data)
+	{
+		list.addNodeFirst(data);
+	}
+	public void deQueue()
+	{
+		list.deleteNodeFirst();
+	}
+	public void display()
+	{
+		list.display();
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Stack and Queue Data structure");
+		QueueDequeue queue = new QueueDequeue();
+		queue.enQueue(70);
+		queue.enQueue(30);
+		queue.enQueue(56);
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		
+	}
+}
